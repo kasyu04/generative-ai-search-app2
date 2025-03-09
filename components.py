bac6625f701247924843a8f1cca47caac32d688e
kasyu04 <kasyu04@gmail.com>
2025-03-09 22:09:19 +0900

    first commit.

--- components.py
+++ components.py
@@ -24,15 +24,21 @@ def display_select_mode():
     """
     回答モードのラジオボタンを表示
     """
-    # 回答モードを選択する用のラジオボタンを表示
-    col1, col2 = st.columns([100, 1])
-    with col1:
-        # 「label_visibility="collapsed"」とすることで、ラジオボタンを非表示にする
-        st.session_state.mode = st.radio(
-            label="",
+    with st.sidebar:
+        st.radio(
+            "利用目的を選択してください",
             options=[ct.ANSWER_MODE_1, ct.ANSWER_MODE_2],
-            label_visibility="collapsed"
+            index=0,
+            key="mode"
         )
+        st.markdown("### 【「社内文書検索」をした場合】")
+        st.markdown("入力内容と関連性が高い社内文書のありかを検索できます。")
+        st.markdown("### 【入力例】")
+        st.markdown("- 社員の育成方針に関するMTGの議事録")
+        st.markdown("### 【「社内問い合わせ」を選択した場合】")
+        st.markdown("質問・要望に対して、社内文書の情報を元に回答を得られます。")
+        st.markdown("### 【入力例】")
+        st.markdown("- 人事部に所属している従業員の情報を一覧化して")
 
 
 def display_initial_ai_message():
@@ -40,80 +46,25 @@ def display_initial_ai_message():
     AIメッセージの初期表示
     """
     with st.chat_message("assistant"):
-        st.markdown("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。サイドバーで利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
+        st.markdown("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。上記で利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
         st.markdown("### 【注意】具体的に入力したほうが期待通りの回答を得やすいです")
+        st.markdown("### 【「社内文書検索」をした場合】")
+        st.markdown("入力内容と関連性が高い社内文書のありかを検索できます。")
+        st.markdown("### 【入力例】")
+        st.markdown("- 社員の育成方針に関するMTGの議事録")
+        st.markdown("### 【「社内問い合わせ」を選択した場合】")
+        st.markdown("質問・要望に対して、社内文書の情報を元に回答を得られます。")
+        st.markdown("### 【入力例】")
+        st.markdown("- 人事部に所属している従業員の情報を一覧化して")
 
 
 def display_conversation_log():
     """
     会話ログの一覧表示
     """
-    # 会話ログのループ処理
     for message in st.session_state.messages:
-        # 「message」辞書の中の「role」キーには「user」か「assistant」が入っている
         with st.chat_message(message["role"]):
-
-            # ユーザー入力値の場合、そのままテキストを表示するだけ
-            if message["role"] == "user":
-                st.markdown(message["content"])
-            
-            # LLMからの回答の場合
-            else:
-                # 「社内文書検索」の場合、テキストの種類に応じて表示形式を分岐処理
-                if message["content"]["mode"] == ct.ANSWER_MODE_1:
-                    
-                    # ファイルのありかの情報が取得できた場合（通常時）の表示処理
-                    if not "no_file_path_flg" in message["content"]:
-                        # ==========================================
-                        # ユーザー入力値と最も関連性が高いメインドキュメントのありかを表示
-                        # ==========================================
-                        # 補足文の表示
-                        st.markdown(message["content"]["main_message"])
-
-                        # 参照元のありかに応じて、適したアイコンを取得
-                        icon = utils.get_source_icon(message['content']['main_file_path'])
-                        # 参照元ドキュメントのページ番号が取得できた場合にのみ、ページ番号を表示
-                        if "main_page_number" in message["content"]:
-                            st.success(f"{message['content']['main_file_path']}", icon=icon)
-                        else:
-                            st.success(f"{message['content']['main_file_path']}", icon=icon)
-                        
-                        # ==========================================
-                        # ユーザー入力値と関連性が高いサブドキュメントのありかを表示
-                        # ==========================================
-                        if "sub_message" in message["content"]:
-                            # 補足メッセージの表示
-                            st.markdown(message["content"]["sub_message"])
-
-                            # サブドキュメントのありかを一覧表示
-                            for sub_choice in message["content"]["sub_choices"]:
-                                # 参照元のありかに応じて、適したアイコンを取得
-                                icon = utils.get_source_icon(sub_choice['source'])
-                                # 参照元ドキュメントのページ番号が取得できた場合にのみ、ページ番号を表示
-                                if "page_number" in sub_choice:
-                                    st.info(f"{sub_choice['source']}", icon=icon)
-                                else:
-                                    st.info(f"{sub_choice['source']}", icon=icon)
-                    # ファイルのありかの情報が取得できなかった場合、LLMからの回答のみ表示
-                    else:
-                        st.markdown(message["content"]["answer"])
-                
-                # 「社内問い合わせ」の場合の表示処理
-                else:
-                    # LLMからの回答を表示
-                    st.markdown(message["content"]["answer"])
-
-                    # 参照元のありかを一覧表示
-                    if "file_info_list" in message["content"]:
-                        # 区切り線の表示
-                        st.divider()
-                        # 「情報源」の文字を太字で表示
-                        st.markdown(f"##### {message['content']['message']}")
-                        # ドキュメントのありかを一覧表示
-                        for file_info in message["content"]["file_info_list"]:
-                            # 参照元のありかに応じて、適したアイコンを取得
-                            icon = utils.get_source_icon(file_info)
-                            st.info(file_info, icon=icon)
+            st.markdown(message["content"])
 
 
 def display_search_llm_response(llm_response):
